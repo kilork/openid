c2a006c9e7fcd84931560b7631c664c19c8a6020
Alexander Korolev <alexander.korolev.germany@gmail.com>
2021-01-30 23:15:46 +0100

    switched to thiserror from failure

--- src/error.rs
+++ src/error.rs
@@ -116,7 +116,6 @@ impl fmt::Display for ClientError {
             ClientError::Url(ref err) => write!(f, "{}", err),
             ClientError::Reqwest(ref err) => write!(f, "{}", err),
             ClientError::Json(ref err) => write!(f, "{}", err),
-            // ClientError::Parse(ref err) => write!(f, "{}", err),
             ClientError::OAuth2(ref err) => write!(f, "{}", err),
             #[cfg(feature = "uma2")]
             ClientError::Uma2(ref err) => write!(f, "{}", err),
@@ -131,7 +130,6 @@ impl std::error::Error for ClientError {
             ClientError::Url(ref err) => Some(err),
             ClientError::Reqwest(ref err) => Some(err),
             ClientError::Json(ref err) => Some(err),
-            // ClientError::Parse(ref err) => Some(err),
             ClientError::OAuth2(ref err) => Some(err),
             #[cfg(feature = "uma2")]
             ClientError::Uma2(ref err) => Some(err),
@@ -153,133 +151,95 @@ impl_from!(ClientError::Io, std::io::Error);
 impl_from!(ClientError::Url, url::ParseError);
 impl_from!(ClientError::Reqwest, reqwest::Error);
 impl_from!(ClientError::Json, serde_json::Error);
-// impl_from!(ClientError::Parse, ParseError);
 impl_from!(ClientError::OAuth2, OAuth2Error);
 
 pub use biscuit::errors::Error as Jose;
 pub use reqwest::Error as Http;
-// pub use reqwest::UrlError as Url;
 pub use serde_json::Error as Json;
 
-use failure::Fail;
+use thiserror::Error;
 
 #[cfg(feature = "uma2")]
 use crate::uma2::Uma2Error;
 
-#[derive(Debug, Fail)]
+#[derive(Debug, Error)]
 pub enum Error {
-    #[fail(display = "{}", _0)]
-    Jose(#[fail(cause)] Jose),
-    //    #[fail(display = "{}", _0)]
-    //    Oauth(#[fail(cause)] Oauth),
-    #[fail(display = "{}", _0)]
-    Http(#[fail(cause)] Http),
-    // #[fail(display = "{}", _0)]
-    // Url(#[fail(cause)] Url),
-    #[fail(display = "{}", _0)]
-    Json(#[fail(cause)] Json),
-    #[fail(display = "{}", _0)]
-    Decode(#[fail(cause)] Decode),
-    #[fail(display = "{}", _0)]
-    Validation(#[fail(cause)] Validation),
-    #[fail(display = "{}", _0)]
-    Userinfo(#[fail(cause)] Userinfo),
-    #[fail(display = "Url must use TLS: '{}'", _0)]
+    #[error(transparent)]
+    Jose(#[from] Jose),
+    #[error(transparent)]
+    Http(#[from] Http),
+    #[error(transparent)]
+    Json(#[from] Json),
+    #[error(transparent)]
+    Decode(#[from] Decode),
+    #[error(transparent)]
+    Validation(#[from] Validation),
+    #[error(transparent)]
+    Userinfo(#[from] Userinfo),
+    #[error("Url must use TLS: '{0}'")]
     Insecure(::reqwest::Url),
-    #[fail(display = "Scope must contain Openid")]
+    #[error("Scope must contain Openid")]
     MissingOpenidScope,
-    #[fail(display = "Url: Path segments is cannot-be-a-base")]
+    #[error("Url: Path segments is cannot-be-a-base")]
     CannotBeABase,
-    #[fail(display = "{}", _0)]
-    ClientError(#[fail(cause)] ClientError),
+    #[error(transparent)]
+    ClientError(#[from] ClientError),
 }
 
-macro_rules! from {
-    ($from:ident) => {
-        impl From<$from> for Error {
-            fn from(e: $from) -> Self {
-                Error::$from(e)
-            }
-        }
-    };
-}
-
-from!(Jose);
-from!(Json);
-// from!(Oauth);
-from!(Http);
-from!(ClientError);
-// from!(Url);
-from!(Decode);
-from!(Validation);
-from!(Userinfo);
-
-#[derive(Debug, Fail)]
+#[derive(Debug, Error)]
 pub enum Decode {
-    #[fail(display = "Token Missing a Key Id when the key set has multiple keys")]
+    #[error("Token Missing a Key Id when the key set has multiple keys")]
     MissingKid,
-    #[fail(display = "Token wants this key id not in the key set: {}", _0)]
+    #[error("Token wants this key id not in the key set: {0}")]
     MissingKey(String),
-    #[fail(display = "JWK Set is empty")]
+    #[error("JWK Set is empty")]
     EmptySet,
 }
 
-#[derive(Debug, Fail)]
+#[derive(Debug, Error)]
 pub enum Validation {
-    #[fail(display = "{}", _0)]
-    Mismatch(#[fail(cause)] Mismatch),
-    #[fail(display = "{}", _0)]
-    Missing(#[fail(cause)] Missing),
-    #[fail(display = "{}", _0)]
-    Expired(#[fail(cause)] Expiry),
+    #[error(transparent)]
+    Mismatch(#[from] Mismatch),
+    #[error(transparent)]
+    Missing(#[from] Missing),
+    #[error(transparent)]
+    Expired(#[from] Expiry),
 }
 
-#[derive(Debug, Fail)]
+#[derive(Debug, Error)]
 pub enum Mismatch {
-    #[fail(
-        display = "Client ID and Token authorized party mismatch: '{}', '{}'",
-        expected, actual
-    )]
+    #[error("Client ID and Token authorized party mismatch: '{expected}', '{actual}'")]
     AuthorizedParty { expected: String, actual: String },
-    #[fail(
-        display = "Configured issuer and token issuer mismatch: '{}' '{}'",
-        expected, actual
-    )]
+    #[error("Configured issuer and token issuer mismatch: '{expected}', '{actual}'")]
     Issuer { expected: String, actual: String },
-    #[fail(
-        display = "Given nonce does not match token nonce: '{}', '{}'",
-        expected, actual
-    )]
+    #[error("Given nonce does not match token nonce: '{expected}', '{actual}'")]
     Nonce { expected: String, actual: String },
 }
 
-#[derive(Debug, Fail)]
+#[derive(Debug, Error)]
 pub enum Missing {
-    #[fail(display = "Token missing Audience")]
+    #[error("Token missing Audience")]
     Audience,
-    #[fail(display = "Token missing AZP")]
+    #[error("Token missing AZP")]
     AuthorizedParty,
-    #[fail(display = "Token missing Auth Time")]
+    #[error("Token missing Auth Time")]
     AuthTime,
-    #[fail(display = "Token missing Nonce")]
+    #[error("Token missing Nonce")]
     Nonce,
 }
 
-#[derive(Debug, Fail)]
+#[derive(Debug, Error)]
 pub enum Expiry {
-    #[fail(display = "Token expired at: {}", _0)]
+    #[error("Token expired at: {0}")]
     Expires(::chrono::naive::NaiveDateTime),
-    #[fail(display = "Token is too old: {}", _0)]
+    #[error("Token is too old: {0}")]
     MaxAge(::chrono::Duration),
 }
 
-#[derive(Debug, Fail)]
+#[derive(Debug, Error)]
 pub enum Userinfo {
-    #[fail(display = "Config has no userinfo url")]
+    #[error("Config has no userinfo url")]
     NoUrl,
-    #[fail(
-        display = "Token and Userinfo Subjects mismatch: '{}', '{}'",
-        expected, actual
-    )]
+    #[error("Token and Userinfo Subjects mismatch: '{expected}', '{actual}'")]
     MismatchSubject { expected: String, actual: String },
 }
