06c62e523d399fc1dc03d2fe944403c33c364e30
Alexander Korolev <alexander.korolev.germany@gmail.com>
2024-04-07 15:34:56 +0200

    make Bearer serialization/deserialization more predictable

--- src/bearer.rs
+++ src/bearer.rs
@@ -1,6 +1,6 @@
 use chrono::{DateTime, Duration, Utc};
-use serde::{de::Visitor, ser::Serializer, Deserialize, Deserializer, Serialize};
-use std::fmt;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 
 /// The bearer token type.
 ///
@@ -8,73 +8,78 @@ use std::fmt;
 #[derive(Deserialize, Serialize, Debug, Clone, PartialEq, Eq)]
 pub struct Bearer {
     pub access_token: String,
+    pub token_type: String,
     pub scope: Option<String>,
+    pub state: Option<String>,
     pub refresh_token: Option<String>,
-    #[serde(
-        default,
-        rename = "expires_in",
-        deserialize_with = "expire_in_to_instant",
-        serialize_with = "serialize_expire_in"
-    )]
-    pub expires: Option<DateTime<Utc>>,
+    pub expires_in: Option<u64>,
     pub id_token: Option<String>,
+    #[serde(flatten)]
+    pub extra: Option<HashMap<String, serde_json::Value>>,
 }
 
-fn expire_in_to_instant<'de, D>(deserializer: D) -> Result<Option<DateTime<Utc>>, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    struct ExpireInVisitor;
-
-    impl<'de> Visitor<'de> for ExpireInVisitor {
-        type Value = Option<DateTime<Utc>>;
-
-        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-            formatter.write_str("an integer containing seconds")
-        }
-
-        fn visit_none<E>(self) -> Result<Self::Value, E>
-        where
-            E: serde::de::Error,
-        {
-            Ok(None)
-        }
-
-        fn visit_some<D>(self, d: D) -> Result<Option<DateTime<Utc>>, D::Error>
-        where
-            D: Deserializer<'de>,
-        {
-            let expire_in: u64 = serde::de::Deserialize::deserialize(d)?;
-            Ok(Some(Utc::now() + Duration::seconds(expire_in as i64)))
-        }
-    }
-
-    deserializer.deserialize_option(ExpireInVisitor)
+/// Manages bearer tokens along with their expiration times.
+pub struct TemporalBearerGuard {
+    bearer: Bearer,
+    expires_at: Option<DateTime<Utc>>,
 }
 
-fn serialize_expire_in<S: Serializer>(
-    dt: &Option<DateTime<Utc>>,
-    serializer: S,
-) -> Result<S::Ok, S::Error> {
-    match dt {
-        Some(dt) => serializer.serialize_some(&dt.timestamp()),
-        None => serializer.serialize_none(),
-    }
-}
-
-impl Bearer {
+impl TemporalBearerGuard {
     pub fn expired(&self) -> bool {
-        if let Some(expires) = self.expires {
-            expires < Utc::now()
-        } else {
-            false
-        }
+        self.expires_at
+            .map(|expires_at| Utc::now() >= expires_at)
+            .unwrap_or_default()
+    }
+
+    pub fn expires_at(&self) -> Option<DateTime<Utc>> {
+        self.expires_at
+    }
+}
+
+impl AsRef<Bearer> for TemporalBearerGuard {
+    fn as_ref(&self) -> &Bearer {
+        &self.bearer
+    }
+}
+
+impl From<Bearer> for TemporalBearerGuard {
+    fn from(bearer: Bearer) -> Self {
+        let expires_at = bearer
+            .expires_in
+            .map(|expires_in| Utc::now() + Duration::seconds(expires_in as i64));
+        Self { bearer, expires_at }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn from_successful_response() {
+        let json = r#"
+        {
+            "access_token":"2YotnFZFEjr1zCsicMWpAA",
+            "token_type":"example",
+            "expires_in":3600,
+            "refresh_token":"tGzv3JOkF0XG5Qx2TlKWIA",
+            "example_parameter":"example_value"
+        }
+        "#;
+        let bearer: Bearer = serde_json::from_str(json).unwrap();
+        assert_eq!("2YotnFZFEjr1zCsicMWpAA", bearer.access_token);
+        assert_eq!("example", bearer.token_type);
+        assert_eq!(Some(3600), bearer.expires_in);
+        assert_eq!(Some("tGzv3JOkF0XG5Qx2TlKWIA".into()), bearer.refresh_token);
+        assert_eq!(
+            Some(
+                [("example_parameter".into(), "example_value".into())]
+                    .into_iter()
+                    .collect()
+            ),
+            bearer.extra
+        );
+    }
 
     #[test]
     fn from_response_refresh() {
@@ -90,9 +95,7 @@ mod tests {
         assert_eq!("aaaaaaaa", bearer.access_token);
         assert_eq!(None, bearer.scope);
         assert_eq!(Some("bbbbbbbb".into()), bearer.refresh_token);
-        let expires = bearer.expires.unwrap();
-        assert!(expires > (Utc::now() + Duration::seconds(3599)));
-        assert!(expires <= (Utc::now() + Duration::seconds(3600)));
+        assert_eq!(Some(3600), bearer.expires_in);
     }
 
     #[test]
@@ -107,6 +110,6 @@ mod tests {
         assert_eq!("aaaaaaaa", bearer.access_token);
         assert_eq!(None, bearer.scope);
         assert_eq!(None, bearer.refresh_token);
-        assert_eq!(None, bearer.expires);
+        assert_eq!(None, bearer.expires_in);
     }
 }
