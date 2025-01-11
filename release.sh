a45e77276f98806fb652eb0e01e02dc99dee4419
Alexander Korolev <alexander.korolev.germany@gmail.com>
2025-01-12 00:48:36 +0100

    CI fixes

--- release.sh
+++ release.sh
@@ -6,12 +6,13 @@ RELEASE_TYPE=${RELEASE_TYPE:-minor}
 cargo set-version --bump ${RELEASE_TYPE}
 VERSION=`cargo pkgid | cut -d"#" -f2`
 export OPENID_RUST_MAJOR_VERSION=`echo ${VERSION} | cut -d"." -f1,2`
-if [ "" != "patch"]; then
+if [ "${RELEASE_TYPE}" != "patch" ]; then
     pushd ../openid-examples
     git checkout main
     git pull
     cargo upgrade -p openid@${OPENID_RUST_MAJOR_VERSION}
     cargo update
+    cargo build
     git add .
     git commit -m"openid version ${OPENID_RUST_MAJOR_VERSION}"
     git branch v${OPENID_RUST_MAJOR_VERSION}
