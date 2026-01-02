e80227b7e804516b9ca9677c4d7f84f7b2c4a747
Alexander Korolev <alexander.korolev.germany@gmail.com>
2026-01-02 21:02:42 +0100

    removed duplicated sub fields from standard claims (#74)

--- src/userinfo.rs
+++ src/userinfo.rs
@@ -9,46 +9,51 @@ use crate::{deserializers::bool_from_str_or_bool, Address, StandardClaimsSubject
 /// scope.
 ///
 /// See: [OpenID Connect Core 1.0: Standard Claims](https://openid.net/specs/openid-connect-core-1_0.html#StandardClaims)
-#[derive(Debug, Deserialize, Serialize, Validate, Clone, Eq, PartialEq)]
+#[derive(Debug, Default, Deserialize, Serialize, Validate, Clone, Eq, PartialEq)]
 pub struct Userinfo {
-    /// Subject - Identifier for the End-User at the Issuer.
-    #[serde(default)]
-    pub sub: Option<String>,
+    /// Subject Identifier.
+    ///
+    /// A locally unique and never reassigned identifier within the Issuer for
+    /// the End-User, which is intended to be consumed by the Client, e.g.,
+    /// `24400320` or `AItOawmwtWwcT0k51BayewNvutrJUqsvl6qs7A4`. It MUST NOT
+    /// exceed 255 ASCII [RFC20] characters in length. The `sub` value is a
+    /// case-sensitive string.
+    pub sub: String,
     /// End-User's full name in displayable form including all name parts,
     /// possibly including titles and suffixes, ordered according to the
     /// End-User's locale and preferences.
-    #[serde(default)]
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub name: Option<String>,
     /// Given name(s) or first name(s) of the End-User. Note that in some
     /// cultures, people can have multiple given names; all can be present, with
     /// the names being separated by space characters.
-    #[serde(default)]
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub given_name: Option<String>,
     /// Surname(s) or last name(s) of the End-User. Note that in some cultures,
     /// people can have multiple family names or no family name; all can be
     /// present, with the names being separated by space characters.
-    #[serde(default)]
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub family_name: Option<String>,
     /// Middle name(s) of the End-User. Note that in some cultures, people can
     /// have multiple middle names; all can be present, with the names being
     /// separated by space characters. Also note that in some cultures, middle
     /// names are not used.
-    #[serde(default)]
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub middle_name: Option<String>,
     /// Casual name of the End-User that may or may not be the same as the
     /// given_name. For instance, a nickname value of Mike might be returned
     /// alongside a given_name value of Michael.
-    #[serde(default)]
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub nickname: Option<String>,
     /// Shorthand name by which the End-User wishes to be referred to at the RP,
     /// such as janedoe or j.doe. This value MAY be any valid JSON string
     /// including special characters such as @, /, or whitespace. The RP MUST
     /// NOT rely upon this value being unique, as discussed in Section 5.7.
-    #[serde(default)]
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub preferred_username: Option<String>,
     /// URL of the End-User's profile page. The contents of this Web page SHOULD
     /// be about the End-User.
-    #[serde(default)]
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub profile: Option<Url>,
     /// URL of the End-User's profile picture. This URL MUST refer to an image
     /// file (for example, a PNG, JPEG, or GIF image file), rather than to a Web
@@ -56,17 +61,17 @@ pub struct Userinfo {
     /// reference a profile photo of the End-User suitable for displaying when
     /// describing the End-User, rather than an arbitrary photo taken by the
     /// End-User.
-    #[serde(default)]
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub picture: Option<Url>,
     /// URL of the End-User's Web page or blog. This Web page SHOULD contain
     /// information published by the End-User or an organization that the
     /// End-User is affiliated with.
-    #[serde(default)]
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub website: Option<Url>,
     /// End-User's preferred e-mail address. Its value MUST conform to the RFC
     /// 5322 [RFC5322] addr-spec syntax. The RP MUST NOT rely upon this value
     /// being unique, as discussed in Section 5.7.
-    #[serde(default)]
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     #[validate(email)]
     pub email: Option<String>,
     /// True if the End-User's e-mail address has been verified; otherwise
@@ -82,7 +87,7 @@ pub struct Userinfo {
     /// End-User's gender. Values defined by this specification are female and
     /// male. Other values MAY be used when neither of the defined values are
     /// applicable.
-    #[serde(default)]
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub gender: Option<String>,
     // ISO 9601:2004 YYYY-MM-DD or YYYY.
     /// End-User's birthday, represented as an ISO 8601:2004 [ISO8601‑2004]
@@ -91,12 +96,12 @@ pub struct Userinfo {
     /// on the underlying platform's date related function, providing just year
     /// can result in varying month and day, so the implementers need to take
     /// this factor into account to correctly process the dates.
-    #[serde(default)]
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub birthdate: Option<NaiveDate>,
     // Region/City codes. Should also have a more concrete serializer form.
     /// String from zoneinfo [zoneinfo] time zone database representing the
     /// End-User's time zone. For example, Europe/Paris or America/Los_Angeles.
-    #[serde(default)]
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub zoneinfo: Option<String>,
     // Usually RFC5646 langcode-countrycode, maybe with a _ sep, could be arbitrary
     /// End-User's locale, represented as a BCP47 [RFC5646] language tag. This
@@ -106,7 +111,7 @@ pub struct Userinfo {
     /// note, some implementations have used an underscore as the separator
     /// rather than a dash, for example, en_US; Relying Parties MAY choose to
     /// accept this locale syntax as well.
-    #[serde(default)]
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub locale: Option<String>,
     // Usually E.164 format number
     /// End-User's preferred telephone number. E.164 [E.164] is RECOMMENDED as
@@ -114,7 +119,7 @@ pub struct Userinfo {
     /// 2400. If the phone number contains an extension, it is RECOMMENDED that
     /// the extension be represented using the RFC 3966 [RFC3966] extension
     /// syntax, for example, +1 (604) 555-1234;ext=5678.
-    #[serde(default)]
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub phone_number: Option<String>,
     /// True if the End-User's phone number has been verified; otherwise false.
     /// When this Claim Value is true, this means that the OP took affirmative
@@ -129,20 +134,18 @@ pub struct Userinfo {
     /// End-User's preferred postal address. The value of the address member is
     /// a JSON [RFC4627] structure containing some or all of the members defined
     /// in Section 5.1.1.
-    #[serde(default)]
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub address: Option<Address>,
     /// Time the End-User's information was last updated. Its value is a JSON
     /// number representing the number of seconds from 1970-01-01T0:0:0Z as
     /// measured in UTC until the date/time.
-    #[serde(default)]
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub updated_at: Option<i64>,
 }
 
 impl StandardClaimsSubject for Userinfo {
     fn sub(&self) -> Result<&str, crate::error::StandardClaimsSubjectMissing> {
-        self.sub
-            .as_deref()
-            .ok_or(crate::error::StandardClaimsSubjectMissing)
+        Ok(self.sub.as_ref())
     }
 }
 
